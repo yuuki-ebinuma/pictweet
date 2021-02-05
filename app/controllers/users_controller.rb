2353ea7a8383d63f2cde5ed6531862e70f800609
yuuki-ebinuma <78467979+yuuki-ebinuma@users.noreply.github.com>
2021-02-05 18:53:24 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  def show 
+    user= User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tweets do
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
+  root to: 'tweets#index'
+  resources :users, only: :show
+end
