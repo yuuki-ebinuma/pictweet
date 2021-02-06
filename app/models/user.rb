6a35a70aa59b8a3d0f770aa12f022b404b45ec7c
yuuki-ebinuma <78467979+yuuki-ebinuma@users.noreply.github.com>
2021-02-06 16:10:41 +0900

    Userモデルの単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :tweets
   has_many :comments
-
+  
   validates :nickname, presence: true, length: { maximum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,25 +6,60 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'nicknameが6文字以下であれば登録できる' do
+        @user.nickname = 'aaaaaa'
+        expect(@user).to be_valid
+      end
     end
-    it 'nicknameが6文字以下であれば登録できる' do
-    end
-    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
-    end
-    it 'nicknameが空では登録できない' do
-    end
-    it 'emailが空では登録できない' do
-    end
-    it 'passwordが空では登録できない' do
-    end
-    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
-    end
-    it 'nicknameが7文字以上では登録できない' do
-    end
-    it '重複したemailが存在する場合登録できない' do
-    end
-    it 'passwordが5文字以下では登録できない' do
+    context '新規登録できないとき' to
+      it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
+        expect(@user).to be_valid
+      end
+      it 'nicknameが空では登録できない' do
+        
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+          end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'nicknameが7文字以上では登録できない' do
+        @user.nickname = 'aaaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 6 characters)")
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
     end
   end
 end
